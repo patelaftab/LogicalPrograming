ab412a5a2550880241dbfccde8564dad1962ec7e
patelaftab <10patelaftab@gmail.com>
2023-02-08 08:56:59 +0530

    [Aftab] :Checking a fibonacci Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            Console.WriteLine("Enter a Number");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int n1 = 0, n2 = 1, n3 = 0;
+            Console.WriteLine(n1 +"\n" + n2 + " ");
+            for(int i=2; i<num; i++) 
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Logical Programing Programs\n" +
+                "Choose a given Option \n" +
+                "1: Fibonacci Series Program");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    fibonacciSeries.Fibonacci();
+                    break;
+                default:
+                    Console.WriteLine("Choose a Correct Option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
